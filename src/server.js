c40bc71c154e866329db7d9755c01e6c0ec7e0f5
Jessica <jliporace@poli.ufrj.br>
2020-06-06 22:37:38 -0300

    created server and reorganized files

---
+++ src/server.js
@@ -0,0 +1,23 @@
+// npm init -y
+// npm install express
+// npm install nodemon -D
+
+const express = require("express")
+const server = express()
+// configurar pasta publica
+server.use(express.static("public"))
+
+//configurar caminhos da minha aplicação
+
+//página inicial
+server.get("/", (req,res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+server.get("/create-point", (req,res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
+})
+
+//ligar o servidor
+server.listen(3000)
+
