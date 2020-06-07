1f5acba7501425d530daa1e611ce260a6439161b
Jessica <jliporace@poli.ufrj.br>
2020-06-07 00:11:44 -0300

    re-estructured all html files using nunjucks

--- src/server.js
+++ src/server.js
@@ -7,15 +7,28 @@ const server = express()
 // configurar pasta publica
 server.use(express.static("public"))
 
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+
+
 //configurar caminhos da minha aplicação
 
 //página inicial
 server.get("/", (req,res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html",{title: "Seu marketplace de coleta de resíduos"})
 })
 
 server.get("/create-point", (req,res) => {
-    res.sendFile(__dirname + "/views/create-point.html")
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req,res) => {
+    return res.render("search-results.html")
 })
 
 //ligar o servidor
